6f8246aca419a8c3b76c42188ea86d6ac550f3a7
Emil Ohlsson <kotte@kottland.net>
2021-11-20 12:21:45 +0100

    Outline GPIO work more

--- gpio.hpp
+++ gpio.hpp
@@ -2,6 +2,11 @@
 
 #include <fmt/core.h>
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 namespace Gpio {
 
 enum class Active {
@@ -18,7 +23,22 @@ struct Output {
         Output &gpio;
     };
 
-    Output(Active level, uint32_t pin) : level(level), pin(pin) {}
+    Output(Active level, uint32_t pin) : level(level), pin(pin) {
+        int fd = open("/sys/class/gpio/export", O_WRONLY);
+        // Write  pin number to fd
+        // TODO can fmtlib create strings?
+        if (fd >= 0) close(fd);
+
+        // TODO open "/sys/class/gpio/gpio<pin>/direction
+        // TODO write "out" to file
+        // TODO
+        // TODO open "/sys/class/gpio/gpio<pin>/value", and keep open
+    }
+
+    ~Output() {
+        // close `value` fd
+        // open "sys/class/unexport" and write pin number
+    }
 
     ActiveScope keep_active() { return ActiveScope(*this); }
 
@@ -33,6 +53,11 @@ struct Output {
 
 struct Input {
     // TODO: Create something that would be usable by future
+    Input(/* pin number */) {
+        // Open as above and export pin, set it as "in"
+        // Open value file and keep open
+        // TODO check how to support C++ futures
+    }
 };
 
 } // namespace Gpio
